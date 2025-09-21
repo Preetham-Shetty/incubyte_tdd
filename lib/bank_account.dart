d3430353fd678ba3cec36764c0cc3f645f1320dc
Preetham Shetty <preetham.shetty6002@gmail.com>
2025-09-21 21:41:29 +0530

    feat: implement withdraw with overdraft protection

--- lib/bank_account.dart
+++ lib/bank_account.dart
@@ -6,4 +6,10 @@ class BankAccount {
     if (amount <= 0) throw ArgumentError('Deposit must be positive');
     _balance += amount;
   }
+
+  void withdraw(int amount) {
+    if (amount <= 0) throw ArgumentError('Withdraw must be positive');
+    if (amount > _balance) throw Exception('Insufficient funds');
+    _balance -= amount;
+  }
 }
