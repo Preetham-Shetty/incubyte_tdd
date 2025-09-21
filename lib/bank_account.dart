d9e7329d21819e5afe012dc28b3643bbd29b5750
Preetham Shetty <preetham.shetty6002@gmail.com>
2025-09-21 21:02:03 +0530

    feat: implement deposit with validation

--- lib/bank_account.dart
+++ lib/bank_account.dart
@@ -1,5 +1,9 @@
 class BankAccount {
   int _balance = 0;
-
   int get balance => _balance;
+
+  void deposit(int amount) {
+    if (amount <= 0) throw ArgumentError('Deposit must be positive');
+    _balance += amount;
+  }
 }
